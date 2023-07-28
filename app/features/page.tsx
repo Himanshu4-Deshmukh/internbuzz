8033273ab1a255c1e7aa24b08e3d0741026d60e7
Himanshu4-Deshmukh <himanshuudeshmukh@gmail.com>
2023-07-28 13:24:43 +0530

    j

---
+++ app/features/page.tsx
@@ -0,0 +1,18 @@
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Features from "@/components/Features";
+
+const features = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="Our Services "
+        description="We serve you as your comfort"
+      />
+
+      <Features/>
+      
+    </>
+  );
+};
+
+export default features;
