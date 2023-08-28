6a7c9da0a0351e467985df0dd3f136a34c936652
Himanshu4-Deshmukh <himanshuudeshmukh@gmail.com>
2023-08-28 22:04:20 +0530

    1st

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -54,14 +54,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/InternBuzz.png"
+                  src="/images/logo/info.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/InternBuzz.png"
+                  src="/images/logo/info.png"
                   alt="logo"
                   width={140}
                   height={30}
